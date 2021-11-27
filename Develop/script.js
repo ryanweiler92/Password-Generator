693d6b96c60b37c3cbb4253ba8909a865219ab23
ryanweiler92 <ryanweiler92@gmail.com>
2021-11-27 14:14:16 -0500

    added object

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,8 @@
-// Assignment code here
-
-
-// Get references to the #generate element
-
-
 var numbers = /^[0-9]+$/
-
+var password = "tEsTiNg123!!"
 //function that asks the user for the length of their password
+
+
 var lengthQuestion = function() {
 var promptLength = window.prompt("How many characters would you like your password to be? Passwords can be between 8 and 128 characters.");
   promptLength = parseInt(promptLength);
@@ -21,6 +17,7 @@ var promptLength = window.prompt("How many characters would you like your passwo
     return lengthQuestion();
   } else {
     console.log(promptLength);
+    passwordInfo.length = promptLength;
     return promptLength;
   }
 };
@@ -35,14 +32,19 @@ var caseQuestion = function() {
     case 1:
       window.alert("You have chosen lowercase characters.");
       console.log(casePrompt);
+      password = password.toLowerCase();
+      passwordInfo.case = casePrompt;
       break;
     case 2: 
       window.alert("You have chosen uppercase characters.");
       console.log(casePrompt);
+      password = password.toUpperCase();
+      passwordInfo.case = casePrompt;
       break;
     case 3:
       window.alert("You have chosen both upper and lowercase characters.");
       console.log(casePrompt);
+      passwordInfo.case = casePrompt;
       break;
     default:
       window.alert("You did not pick a valid option. Please try again.");
@@ -56,10 +58,12 @@ var numericQuestion = function() {
   if (confirmNumeric) {
     window.confirm("You have chosen to include numeric characters in your password.")
     console.log(confirmNumeric);
+    passwordInfo.numeric = confirmNumeric;
     return true;
 } else {
   window.alert("You have chosen to not include numeric characters in your password.")
   console.log(confirmNumeric);
+  passwordInfo.numeric = confirmNumeric;
   return false;
 }
 };
@@ -69,10 +73,12 @@ var specialCharactersQuestion = function() {
   if (confirmSpecialCharacter) {
     window.alert("You have chosen to include special characters in your password.")
     console.log(confirmSpecialCharacter);
+    passwordInfo.special = confirmSpecialCharacter;
     return true;
   } else {
     window.alert("You have chosen to not include special characters in your password.");
     console.log(confirmSpecialCharacter);
+    passwordInfo.special = confirmSpecialCharacter;
     return false;
   }
 };
@@ -100,22 +106,31 @@ function writePassword() {
  
     window.alert("Welcome to Password Generator, please click OK to continue!");
     window.alert("Please answer the following questions to generate a password. Click OK to continue.");
-    //ask user how long they would like their password to be
+    
     lengthQuestion();
     caseQuestion();
     numericQuestion();
     specialCharactersQuestion();
+    console.log(passwordInfo)
 
 
  //var password = generatePassword();
  var passwordText = document.querySelector("#password");
- passwordText.innerHTML = "test12345";
+ passwordText.innerHTML = password;
  
  //console.log(lengthQuestion.value);
  //console.log(lengthQuestion);
  //console.log(lengthPrompt.value);
  //console.log(lengthPrompt);
 
+}
+
+
+var passwordInfo = {
+  length: 0,
+  case: 0,
+  numeric: false,
+  special: false
 }
 
 
